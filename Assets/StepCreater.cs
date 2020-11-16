c803170a381736430c5fa26902a083e819af1b0f
Daisuke Tsukada <dsgarage@gmail.com>
2020-11-16 17:45:12 +0900

    Change Code

--- Assets/StepCreater.cs
+++ Assets/StepCreater.cs
@@ -3,6 +3,7 @@ using UnityEditor;
 using System.Collections;
  
 public class StepCreater : EditorWindow {
+    
     private GameObject parent;
     private GameObject prefab;
     private int numX = 1;
@@ -14,15 +15,14 @@ public class StepCreater : EditorWindow {
  
     [MenuItem("GameObject/Create Other/Create Steps")]
     static void Init() {
-        EditorWindow.GetWindow<StepCreater>(true, "Create Buoys");
+        EditorWindow.GetWindow<StepCreater>(true, "Step");
     }
  
     void OnEnable() {
-        if (Selection.gameObjects.Length > 0) parent = Selection.gameObjects[0];
+
     }
  
     void OnSelectionChange() {
-        if (Selection.gameObjects.Length > 0) prefab = Selection.gameObjects[0];
         Repaint();
     }
  
@@ -49,21 +49,27 @@ public class StepCreater : EditorWindow {
     }
  
     private void Create() {
-        if (prefab == null) return;
+        if (prefab == null)
+        {
+            return;
+        }
  
         int count = 0;
         Vector3 pos;
  
         pos.x = -(numX - 1) * intervalX / 2;
+        
         for (int x = 0; x < numX; x++) {
             pos.y = -(numY - 1) * intervalY / 2;
+            
             for (int y = 0; y < numY; y++) {
                 pos.z = -(numZ - 1) * intervalZ / 2;
+
                 for (int z = 0; z < numZ; z++) {
                     GameObject obj = Instantiate(prefab, pos, Quaternion.identity) as GameObject;
                     obj.name = prefab.name + count++;
                     if (parent) obj.transform.parent = parent.transform;
-                    Undo.RegisterCreatedObjectUndo(obj, "Create Buoys");
+                    Undo.RegisterCreatedObjectUndo(obj, "Step");
  
                     pos.z += intervalZ;
                 }
