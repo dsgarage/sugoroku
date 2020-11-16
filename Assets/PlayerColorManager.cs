520c8b7f27f9eaa795f62f9bb2ed5e3fd8eb513c
Daisuke Tsukada <dsgarage@gmail.com>
2020-11-16 19:10:38 +0900

    Add PlayerColorManager.

---
+++ Assets/PlayerColorManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerColorManager : MonoBehaviour
+{
+    [SerializeField]private Color playerColor = Color.red;
+
+    private void Start()
+    {
+        // Create a material with transparent diffuse shader
+        Material material = new Material(Shader.Find("Transparent/Diffuse"));
+        material.color = playerColor;
+
+        // assign the material to the renderer
+        GetComponent<Renderer>().material = material;
+    }
+
+}
