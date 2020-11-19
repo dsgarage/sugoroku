784e37175ec267452609952354c1e4f35889aff2
Daisuke Tsukada <dsgarage@gmail.com>
2020-11-20 00:58:49 +0900

    BlandListManager作成

---
+++ Assets/BlandListManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class BlandListManager : MonoBehaviour
+{
+    [SerializeField]private CinemachineBlendListCamera blendlist;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        blendlist = GetComponent<CinemachineBlendListCamera>();
+        foreach (var VARIABLE in blendlist.m_Instructions)
+        {
+            Debug.Log(VARIABLE.m_VirtualCamera.VirtualCameraGameObject.name);
+        }
+
+        foreach (var VARIABLE in blendlist.ChildCameras)
+        {
+            Debug.Log(VARIABLE.LookAt.root.gameObject.name);
+            Debug.Log(VARIABLE.Follow.root.gameObject.name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
