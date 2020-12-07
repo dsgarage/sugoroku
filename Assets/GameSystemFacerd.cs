f82e2c22023e0cdfd3ff2e2facf118e8e8fb0cb0
Daisuke Tsukada <dsgarage@gmail.com>
2020-12-07 18:55:48 +0900

    ファサードクラス追加

---
+++ Assets/GameSystemFacerd.cs
@@ -0,0 +1,17 @@
+﻿using Cinemachine;
+
+public static class GameSystemFacerd
+{
+    public static CinemachineBlendListCamera GetBlendListCameraObject
+    {
+        get { return _cinemachineBlendListCamera; }
+    }
+    private static CinemachineBlendListCamera _cinemachineBlendListCamera;
+
+    public static void SetBlendListCamera(CinemachineBlendListCamera cinemachineBlendListCamera)
+    {
+        _cinemachineBlendListCamera = cinemachineBlendListCamera;
+    }
+
+}
+ 
